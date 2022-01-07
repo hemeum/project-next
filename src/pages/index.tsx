fbfaf94f3884049bd3bbb5c5fd04736203e50b0b
lamenking <dnwjddus1506@hanmail.net>
2022-01-07 19:17:38 +0900

    Begin project-next

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next project</title>
+      </Head>
+      <h1>시작합니다</h1>
+    </div>
+  );
+};
+
+export default Home;
