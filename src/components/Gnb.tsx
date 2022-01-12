bccea2a756d11e26a693a0419f0941434720fd7f
lamenking <dnwjddus1506@hanmail.net>
2022-01-12 22:33:14 +0900

    💄 Add global navi bar

---
+++ src/components/Gnb.tsx
@@ -0,0 +1,80 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+function Gnb() {
+  return (
+    <Wrap>
+      <Inner>
+        <Logo>
+          <h1 className="gnb-title"></h1>
+          <Link href="/">
+            <a>
+              <img src="/img/logo-stv.png" alt="stove 로고"></img>
+            </a>
+          </Link>
+        </Logo>
+        <ul>
+          <li>PC게임</li>
+          <li>모바일게임</li>
+          <li>인디게임</li>
+        </ul>
+      </Inner>
+      <Service>
+        <i className="fas fa-bars"></i>
+        <p>로그인</p>
+      </Service>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  width: 100%;
+  min-width: 1400px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  height: 70px;
+  ul {
+    display: flex;
+    line-height: 70px;
+    li {
+      font-weight: bold;
+      margin-right: 40px;
+    }
+  }
+`;
+
+const Logo = styled.div`
+  margin-right: 50px;
+  a {
+    display: block;
+    height: 100%;
+    img {
+      width: 120px;
+      height: 50px;
+      margin-top: 10px;
+      object-fit: cover;
+    }
+  }
+`;
+
+const Service = styled.div`
+  display: flex;
+  justify-content: right;
+  margin-right: 30px;
+  line-height: 70px;
+  i {
+    font-size: 20px;
+    line-height: 70px;
+    margin-right: 20px;
+  }
+  p {
+    font-weight: bold;
+  }
+`;
+
+export default Gnb;
