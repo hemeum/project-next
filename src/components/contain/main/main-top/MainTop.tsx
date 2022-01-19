57d51bcf08645c73881084341f891d2944536f0b
lamenking <dnwjddus1506@hanmail.net>
2022-01-19 17:09:36 +0900

    💄 Update scroll props and style

--- src/components/contain/main/main-top/MainTop.tsx
+++ src/components/contain/main/main-top/MainTop.tsx
@@ -3,18 +3,18 @@ import styled from "styled-components";
 import LeftBanners from "./LeftBanners";
 import RightBanners from "./RightBanners";
 
-function MainTop() {
+function MainTop({ scroll }: { scroll: number }) {
   return (
-    <Top>
+    <Top scroll={scroll}>
       <LeftBanners></LeftBanners>
       <RightBanners />
     </Top>
   );
 }
 
-const Top = styled.div`
+const Top = styled.div<{ scroll: number }>`
   position: absolute;
-  top: 370px;
+  top: 300px;
   left: 50%;
   transform: translateX(-50%);
   display: flex;
