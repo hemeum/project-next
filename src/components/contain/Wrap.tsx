a3ebb08e74ea655332eb97f152edd86d8cb6ad9d
lamenking <dnwjddus1506@hanmail.net>
2022-01-26 14:28:50 +0900

    💄 Update Wrap about bg and header

--- src/components/contain/Wrap.tsx
+++ src/components/contain/Wrap.tsx
@@ -3,7 +3,7 @@ import Main from "src/components/contain/main/Main";
 
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-import { indexOf, throttle } from "lodash";
+import { throttle } from "lodash";
 
 function Wrap() {
   const [scroll, setScroll] = useState(0);
@@ -24,16 +24,36 @@ function Wrap() {
   }, []);
 
   return (
-    <WrapBg>
+    <WrapAll>
       <Header scroll={scroll}></Header>
-      <Main scroll={scroll}></Main>
-    </WrapBg>
+      <WrapMain>
+        <Main scroll={scroll}></Main>
+      </WrapMain>
+      <Bg></Bg>
+    </WrapAll>
   );
 }
 
-const WrapBg = styled.div`
+const WrapAll = styled.div`
+  /*WrapAll에서 header와 main이 높이를 나눠가지고, zindex로 높이 뛰움. 그 zindex보다 낮게 bg를 뛰워서 배경이미지를 줬다. 포지션 sticky때문에 이렇게 함. 단점은 부모인 All에게 height를 고정으로 줘야함.*/
   position: relative;
   min-width: 1300px;
+  height: 3400px;
+`;
+
+const WrapMain = styled.div`
+  position: absolute;
+  top: 80px;
+  left: 0;
+  z-index: 100;
+  width: 100%;
+`;
+
+const Bg = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
   height: 1000px;
   background-image: url("/img/main-bg.jpg");
   background-size: cover;
