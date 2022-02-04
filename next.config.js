bab08cc5b7fdac4567de65bd568a72f0d32394fa
lamenking <dnwjddus1506@hanmail.net>
2022-02-04 11:33:22 +0900

    ✨ next with express server proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    // express 서버와 proxy로 연동
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:6000/:path*",
+      },
+    ];
+  },
 };
