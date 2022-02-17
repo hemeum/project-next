b28add890fbf338b4b01bb04cbed489883b3ddca
lamenking <dnwjddus1506@hanmail.net>
2022-02-18 00:07:59 +0900

    🚧 Working go back components

---
+++ src/components/list/TitleTop.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { useRouter } from "next/router";
+export default function TitleTop() {
+  const router = useRouter();
+  // router.query 넣어서 바꿔주자
+  return (
+    <Top>
+      <h2>{router.query.ctg}</h2>
+      <GoBack>
+        <i aria-hidden className="fa-solid fa-bars"></i>
+        <p>목록가기</p>
+      </GoBack>
+    </Top>
+  );
+}
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  border-bottom: 3px solid black;
+`;
+
+const GoBack = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 120px;
+  height: 40px;
+  padding: 5px 15px;
+  border: 1px solid lightgray;
+  font-weight: bold;
+  cursor: pointer;
+  i {
+    margin-right: 5px;
+  }
+`;
