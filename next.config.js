5ea35f182fff26f839c91500b5dbf507578bc2e8
lamenking <dnwjddus1506@hanmail.net>
2022-02-28 15:32:56 +0900

    Update config rewrites

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites(){
+    return [{source:'/path*', destination:'https://project-next-3dedkvr1u-lamenking.vercel.app/path*'}]
+  }
 };
