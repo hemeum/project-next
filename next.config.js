c81cb553561aba6129507c3272528aa4dfd4966f
lamenking <dnwjddus1506@hanmail.net>
2022-02-28 15:41:34 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites(){
-    return [{source:'/:path*', destination:'https://project-next-3dedkvr1u-lamenking.vercel.app/:path*'}]
-  }
+ 
 };
