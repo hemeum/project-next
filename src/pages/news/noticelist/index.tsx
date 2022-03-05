8812db97b48bfa011f6c4371072e73ffb286232c
lamenking <dnwjddus1506@hanmail.net>
2022-03-06 00:53:20 +0900

    🐛 Fix url for vercel app

--- src/pages/news/noticelist/index.tsx
+++ src/pages/news/noticelist/index.tsx
@@ -11,22 +11,25 @@ export default function PageFreeList({ list, pageNumbers }: any) {
 }
 
 export async function getServerSideProps(context: any) {
-  const res = await axios.post("http://localhost:3000/api/post/list", {
-    category: context.query.ctg
-      ? context.query.ctg
-      : context.resolvedUrl ===
-          "/community/freelist?ctg=%EC%9E%90%EC%9C%A0%EA%B2%8C%EC%8B%9C%ED%8C%90&page=1&searchType=&searchText=&orderType=%EC%B5%9C%EC%8B%A0%EC%88%9C" ||
-        context.resolvedUrl === "/community/freelist"
-      ? "자유게시판"
-      : "공지사항",
-    pageNumber: context.query.page ? context.query.page : 1,
-    searchType: context.query.searchType ? context.query.searchType : "",
-    searchText: context.query.searchText ? context.query.searchText : "",
-    orderType:
-      context.query.orderType === "최신순" || !context.query.orderType
-        ? "최신순"
-        : "좋아요순",
-  });
+  const res = await axios.post(
+    "https://project-next-git-api-lamenking.vercel.app/api/post/list",
+    {
+      category: context.query.ctg
+        ? context.query.ctg
+        : context.resolvedUrl ===
+            "/community/freelist?ctg=%EC%9E%90%EC%9C%A0%EA%B2%8C%EC%8B%9C%ED%8C%90&page=1&searchType=&searchText=&orderType=%EC%B5%9C%EC%8B%A0%EC%88%9C" ||
+          context.resolvedUrl === "/community/freelist"
+        ? "자유게시판"
+        : "공지사항",
+      pageNumber: context.query.page ? context.query.page : 1,
+      searchType: context.query.searchType ? context.query.searchType : "",
+      searchText: context.query.searchText ? context.query.searchText : "",
+      orderType:
+        context.query.orderType === "최신순" || !context.query.orderType
+          ? "최신순"
+          : "좋아요순",
+    },
+  );
   let leng = res.data.leng;
   const arr: any = [];
   for (let i = 1; i <= Math.ceil(leng / 10); i++) {
